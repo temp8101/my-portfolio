a90118966c0b22682710ad6d0155094abf69c76b
temp8101 <temp8101@gmail.com>
2018-08-22 09:30:46 -0400

    Updated webpack script

---
+++ webpack-config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+
+	entry : './js/main.js',
+	output: 
+	{
+		path: path.resolve(_dirname,'dist'),
+		filename: 'bundle.js'
+	},
+	module: 
+	{
+		rules: [
+		{
+			test: /\.js$/,
+			exclude: /node_modules/,
+			use: 
+			{
+				loader: 'babel-loader',
+				options: {
+					presets: ['react']
+				}
+			}
+		}
+		]
+	}
+
+}
